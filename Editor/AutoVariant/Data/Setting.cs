923a8411d63e8f2229fab010604eb12ab8b96bbd
4OF <404.fof.nf@gmail.com>
2025-05-29 12:14:46 +0900

    fix: change default value

--- Editor/AutoVariant/Data/Setting.cs
+++ Editor/AutoVariant/Data/Setting.cs
@@ -12,7 +12,7 @@ namespace Untitled.Data.Setting
             { "AutoVariant", new SettingItem[] {
                 new BoolSettingItem("AutoVariant_enableAutoVariant", true),
                 new BoolSettingItem("AutoVariant_enablePrebuild", true),
-                new BoolSettingItem("AutoVariant_includeAllAssets", false)
+                new BoolSettingItem("AutoVariant_includeAllAssets", true)
             } },
         };
     }
