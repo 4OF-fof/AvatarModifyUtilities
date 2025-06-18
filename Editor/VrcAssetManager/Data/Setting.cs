fda49ce8c07ccbb4abe9e841d41cf482ceabfd7d
4OF <404.fof.nf@gmail.com>
2025-06-18 11:38:56 +0900

    refactor: AssetManager

---
+++ Editor/VrcAssetManager/Data/Setting.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+using AMU.Editor.Core.Schema;
+
+namespace AMU.Editor.Setting
+{
+    public static class VrcAssetManagerSettingData
+    {
+        public static readonly Dictionary<string, SettingItem[]> SettingItems = new Dictionary<string, SettingItem[]>
+        {
+            { "VrcAssetManager_category", new SettingItem[] {
+                new BoolSettingItem("AssetManager_watchDownloadFolder", false),
+                new TextAreaSettingItem("AssetManager_excludedImportExtensions", ".zip\n.psd", false, 3, 8)
+            } },
+        };
+    }
+}
