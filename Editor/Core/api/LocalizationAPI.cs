6fb2ef6852ca6b14303319b2ab307ae1e21982cb
4OF <404.fof.nf@gmail.com>
2025-06-21 02:34:48 +0900

    refactor: Core/API

--- Editor/Core/api/LocalizationAPI.cs
+++ Editor/Core/api/LocalizationAPI.cs
@@ -4,6 +4,7 @@ namespace AMU.Editor.Core.Api
 {
     public static class LocalizationAPI
     {
+        public static void LoadLanguage(string lang) => LocalizationController.LoadLanguage(lang);
         public static string GetText(string key) => LocalizationController.GetText(key);
         public static string CurrentLanguage => LocalizationController.CurrentLanguage;
     }
