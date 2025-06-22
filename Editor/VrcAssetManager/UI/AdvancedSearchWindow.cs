89d0e41ddd39e1811d6d601bd9c73999bb636912
4OF <404.fof.nf@gmail.com>
2025-06-22 16:14:19 +0900

    style: adv search

--- Editor/VrcAssetManager/UI/AdvancedSearchWindow.cs
+++ Editor/VrcAssetManager/UI/AdvancedSearchWindow.cs
@@ -31,7 +31,7 @@ namespace AMU.Editor.VrcAssetManager.UI
                 window._tagsAnd = controller.filterOptions.tagsAnd;
                 window._filterAnd = controller.filterOptions.filterAnd;
             }
-            window.minSize = new Vector2(400, 340);
+            window.minSize = window.maxSize = new Vector2(300, 400);
             window.Show();
         }
 
@@ -40,26 +40,58 @@ namespace AMU.Editor.VrcAssetManager.UI
             GUILayout.Label("詳細検索", EditorStyles.boldLabel);
             GUILayout.Space(8);
 
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            // 全体ANDトグル
             _filterAnd = EditorGUILayout.ToggleLeft("全体をANDで絞り込む", _filterAnd);
+            EditorGUILayout.LabelField("検索条件", EditorStyles.boldLabel);
             GUILayout.Space(4);
 
-            _name = EditorGUILayout.TextField("名前", _name);
-            _author = EditorGUILayout.TextField("作者", _author);
-            _description = EditorGUILayout.TextField("説明", _description);
+            // 名前・作者・説明
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label("名前", GUILayout.Width(60));
+            _name = EditorGUILayout.TextField(_name, GUILayout.MinWidth(200));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label("作者", GUILayout.Width(60));
+            _author = EditorGUILayout.TextField(_author, GUILayout.MinWidth(200));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label("説明", GUILayout.Width(60));
+            _description = EditorGUILayout.TextField(_description, GUILayout.MinWidth(200));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.EndVertical();
 
-            GUILayout.Space(8);
-            GUILayout.Label("タグ", EditorStyles.label);
-            using (new GUILayout.HorizontalScope())
+            GUILayout.Space(10);
+
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            GUILayout.Label("タグ", EditorStyles.boldLabel);
+            if (_tags.Count > 0)
             {
-                if (_tags.Count > 0)
+                EditorGUILayout.BeginVertical();
+                foreach (var tag in _tags)
                 {
-                    GUILayout.Label(string.Join(", ", _tags), EditorStyles.textField, GUILayout.ExpandWidth(true));
+                    GUIStyle tagBox = new GUIStyle(EditorStyles.helpBox);
+                    tagBox.fontSize = 12;
+                    tagBox.fixedHeight = 24;
+                    tagBox.alignment = TextAnchor.MiddleCenter;
+                    tagBox.padding = new RectOffset(6, 6, 2, 2);
+                    tagBox.margin = new RectOffset(2, 2, 2, 2);
+                    EditorGUILayout.LabelField(tag, tagBox, GUILayout.ExpandWidth(true));
                 }
-                else
+                EditorGUILayout.EndVertical();
+                if (GUILayout.Button("タグ選択", GUILayout.ExpandWidth(true)))
                 {
-                    GUILayout.Label("(未選択)", EditorStyles.textField, GUILayout.ExpandWidth(true));
+                    TagSelectorWindow.ShowWindow(true, tags =>
+                    {
+                        _tags = tags ?? new List<string>();
+                        Repaint();
+                    }, _tags);
                 }
-                if (GUILayout.Button("タグ選択", GUILayout.Width(80)))
+                _tagsAnd = EditorGUILayout.ToggleLeft("タグをANDで絞り込む", _tagsAnd);
+            }
+            else
+            {
+                if (GUILayout.Button("タグ選択", GUILayout.ExpandWidth(true)))
                 {
                     TagSelectorWindow.ShowWindow(true, tags =>
                     {
@@ -68,17 +100,19 @@ namespace AMU.Editor.VrcAssetManager.UI
                     }, _tags);
                 }
             }
-            _tagsAnd = EditorGUILayout.ToggleLeft("タグをANDで絞り込む", _tagsAnd);
+            EditorGUILayout.EndVertical();
 
             GUILayout.FlexibleSpace();
+            GUILayout.Space(8);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
             using (new GUILayout.HorizontalScope())
             {
-                if (GUILayout.Button("キャンセル"))
+                if (GUILayout.Button("キャンセル", GUILayout.Width(100)))
                 {
                     Close();
                 }
                 GUILayout.FlexibleSpace();
-                if (GUILayout.Button("検索", GUILayout.Width(100)))
+                if (GUILayout.Button("検索", GUILayout.Width(120)))
                 {
                     ApplySearch();
                     Close();
